e00389d682f07ba9a6cc83803c7098801bb2dca2
Rodrigo Azevedo <razevedocosta@gmail.com>
2022-11-20 13:15:51 -0300

    update week-3

---
+++ module-3/week-3/09-abstracao.js
@@ -0,0 +1,41 @@
+class Parafuso {
+    constructor() {
+        if (this.constructor === Parafuso)
+            throw new Error("Classe abstrata não pode ser instanciada");
+    }
+
+    get tipo() {
+        throw new Error("Método get tipo precisa ser implementado");
+    }
+}
+
+class Fenda extends Parafuso {
+    constructor() {
+        super()
+    }
+
+    get tipo() {
+        return "fenda"
+    }
+}
+
+class Philips extends Parafuso {
+    constructor() {
+        super()
+    }
+
+    get tipo() {
+        return "philips"
+    }
+}
+
+class Allen extends Parafuso{}
+
+//criando e usando a classe
+let parafuso = new Parafuso()
+let fenda = new Fenda()
+let philips = new Philips()
+let allen = new Allen()
+
+console.log(fenda.tipo, philips.tipo);
+console.log(allen.tipo);
