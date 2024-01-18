de751f502cb8132b6b824b1568ef785f8f19f083
rodrigo azevedo <razevedocosta@gmail.com>
2024-01-18 19:33:24 -0300

    update project

--- python/consultando-api/github.py
+++ python/consultando-api/github.py
@@ -1,20 +1,17 @@
-# Requisições HTTP com Python e consumindo a API do GitHub
 import requests
 
-print('GitHub Users\n')
+print('Githup Users API\n')
 
 username = input('Qual é o nome do usuário? ')
 
 url = f'https://api.github.com/users/{username}'
-
 response = requests.get(url)
 data = response.json()
 
 if response.status_code == 200:
     print(f'\nNome completo: {data["name"]}')
     print(f'Bio: {data["bio"]}')
-    print(f'Localização: {data["location"]}')
-    print(f'Seguidores: {data["followers"]}')
-    print(f'Seguindo: {data["following"]}')
+    print(f'Cidade: {data["location"]}')
+    print(f'Repositórios: {data["repos_url"]}')
 else:
-    print('Não foi possível encontrar o usuário!')
+    print('Não foi possível encontrar o usuário! =/')
---
+++ python/programa/readme.md
@@ -0,0 +1,4 @@
+criar funções para isolar o código
+criar a função de cadastro
+criar a função de listagem
+refatorando o código
